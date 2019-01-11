0485f98b6c670170a76760327488bcacaaf19f64
litao <litao143>
2019-01-11 15:23:56 +0800

    加入骨架布局

--- popular/src/main/java/com/popular/MainAct.java
+++ popular/src/main/java/com/popular/MainAct.java
@@ -2,14 +2,15 @@ package com.popular;
 
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import com.andexert.library.RippleView;
 import com.ethanhua.skeleton.Skeleton;
 import com.ethanhua.skeleton.SkeletonScreen;
 import com.popular.comm.BaseActivity;
-import com.popular.practice.PracticeAct;
 import java.lang.ref.WeakReference;
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -25,6 +26,8 @@ public class MainAct extends BaseActivity
 	ImageView iv1;
 	@BindView(R.id.iv2)
 	ImageView iv2;
+	@BindView(R.id.more)
+	RippleView rippleView;
 	private SkeletonScreen skeletonScreen;
 	//https://github.com/tyrantgit/ExplosionField
 	private ExplosionField explosionField;
@@ -42,6 +45,20 @@ public class MainAct extends BaseActivity
 	
 	@Override
 	public void initListener(){
+		rippleView.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Log.e("Sample", "Click Rect !");
+			}
+		});
+		rippleView.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
+			@Override
+			public void onComplete(RippleView rippleView) {
+				Log.d("Sample", "Ripple completed");
+			}
+		});
 	}
 	
 	@Override
@@ -91,6 +108,6 @@ public class MainAct extends BaseActivity
 	
 	@OnClick(R.id.btn_practice)
 	public void onClick(){
-		showActivity(this, PracticeAct.class);
+		//showActivity(this, PracticeAct.class);
 	}
 }
