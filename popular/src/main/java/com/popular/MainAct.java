2d33d72d7dfe3550b684ff33fc7ea38ff67d2960
litao <litao143>
2019-02-22 18:27:10 +0800

    加入DialogFragment

--- popular/src/main/java/com/popular/MainAct.java
+++ popular/src/main/java/com/popular/MainAct.java
@@ -1,39 +1,54 @@
 package com.popular;
 
+import android.graphics.Color;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.view.ViewTreeObserver;
+import android.widget.*;
 import com.andexert.library.RippleView;
 import com.ethanhua.skeleton.Skeleton;
 import com.ethanhua.skeleton.SkeletonScreen;
 import com.popular.comm.BaseActivity;
 import com.popular.practice.PracticeAct;
 import com.popular.practice.ui.DialogTestAct;
+import com.popular.practice.ui.view.ObservableScrollView;
 import java.lang.ref.WeakReference;
 import butterknife.BindView;
 import butterknife.OnClick;
 import tyrantgit.explosionfield.ExplosionField;
 
 public class MainAct extends BaseActivity
+		implements ObservableScrollView.OnObservableScrollViewListener
 {
 	@BindView(R.id.btn_practice)
 	Button mBtnPractice;
 	@BindView(R.id.ll_main)
-	LinearLayout mllMain;
+	RelativeLayout mllMain;
 	@BindView(R.id.iv1)
 	ImageView iv1;
 	@BindView(R.id.iv2)
 	ImageView iv2;
 	@BindView(R.id.more)
 	RippleView rippleView;
+	@BindView(R.id.sv_main_content)
+	ObservableScrollView mSvMainContent;
+	@BindView(R.id.iv_header_left)
+	ImageView mIvHeaderLeft;
+	@BindView(R.id.tv_header_title)
+	TextView mTvHeaderTitle;
+	@BindView(R.id.iv_header_img)
+	ImageView mIvHeaderImg;
+	@BindView(R.id.ll_header_content)
+	LinearLayout mLlHeaderContent;
+	@BindView(R.id.img_banner)
+	ImageView img_banner;
 	private SkeletonScreen skeletonScreen;
 	//https://github.com/tyrantgit/ExplosionField
 	private ExplosionField explosionField;
 	
+	private int mHeight;
 	
 	@Override
 	public int getLayoutRes(){
@@ -42,7 +57,7 @@ public class MainAct extends BaseActivity
 	
 	@Override
 	public void initView(){
-		explosionField = ExplosionField.attach2Window(this);
+		explosionField=ExplosionField.attach2Window(this);
 	}
 	
 	@Override
@@ -50,14 +65,14 @@ public class MainAct extends BaseActivity
 		rippleView.setOnClickListener(new View.OnClickListener()
 		{
 			@Override
-			public void onClick(View v)
-			{
+			public void onClick(View v){
 				Log.e("Sample", "Click Rect !");
 			}
 		});
-		rippleView.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
+		rippleView.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener()
+		{
 			@Override
-			public void onComplete(RippleView rippleView) {
+			public void onComplete(RippleView rippleView){
 				Log.d("Sample", "Ripple completed");
 			}
 		});
@@ -74,8 +89,19 @@ public class MainAct extends BaseActivity
 				.show();
 		MyHandler myHandler=new MyHandler(this);
 		myHandler.sendEmptyMessageDelayed(1, 2000);
+		
+		//获取标题栏高度
+		ViewTreeObserver viewTreeObserver = img_banner.getViewTreeObserver();
+		viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+			@Override
+			public void onGlobalLayout() {
+				img_banner.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+				mHeight = img_banner.getHeight() - mLlHeaderContent.getHeight();//这里取的高度应该为图片的高度-标题栏
+				//注册滑动监听
+				mSvMainContent.setOnObservableScrollViewListener(MainAct.this);
+			}
+		});
 	}
-	
 	
 	@OnClick({R.id.iv1, R.id.iv2})
 	public void onClick(View view){
@@ -89,6 +115,34 @@ public class MainAct extends BaseActivity
 			iv2.setVisibility(View.INVISIBLE);
 			break;
 		}
+	}
+	
+	/**
+	 * 获取ObservableScrollView的滑动数据
+	 *
+	 * @param l
+	 * @param t
+	 * @param oldl
+	 * @param oldt
+	 */
+	@Override
+	public void onObservableScrollViewListener(int l, int t, int oldl, int oldt) {
+		if (t <= 0) {
+			//顶部图处于最顶部，标题栏透明
+			mLlHeaderContent.setBackgroundColor(Color.argb(0, 48, 63, 159));
+		} else if (t > 0 && t < mHeight) {
+			//滑动过程中，渐变
+			float scale = (float) t / mHeight;//算出滑动距离比例
+			float alpha = (255 * scale);//得到透明度
+			mLlHeaderContent.setBackgroundColor(Color.argb((int) alpha, 48, 63, 159));
+		} else {
+			//过顶部图区域，标题栏定色
+			mLlHeaderContent.setBackgroundColor(Color.argb(255, 48, 63, 159));
+		}
+	}
+	
+	@Override
+	public void onPointerCaptureChanged(boolean hasCapture){
 	}
 	
 	public static class MyHandler extends Handler
@@ -113,7 +167,7 @@ public class MainAct extends BaseActivity
 		showActivity(this, PracticeAct.class);
 	}
 	
-	public void dialogfm(View view) {
+	public void dialogfm(View view){
 		showActivity(this, DialogTestAct.class);
 	}
 }
